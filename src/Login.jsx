f8b95d0a015976563695fa6924b9e035520266dd
João V. S. Oliveira <145166602+j0t4Vv@users.noreply.github.com>
2024-03-08 17:14:24 -0300

    Add files via upload

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import './main.css'
+import './main.css';
+import './Login.css';
 
 const Login = () => {
   return (
@@ -9,7 +10,12 @@ const Login = () => {
         <div className="header">
           <span>Login</span>
         </div>
-        <div className='conteudo'>
+        <div className='conteudoLogin'>
+          <label htmlFor="email">Email:</label>
+          <input type="text" id="email" name="email" placeholder='Endereço de e-mail'/>
+          <label htmlFor="password">Senha:</label>
+          <input type='password' id="password" name="senha" placeholder='Senha' />
+          <button>Entrar</button>
         <Link to={"/GestaoProdutos"}>
         Gestão de Produtos
         </Link>
