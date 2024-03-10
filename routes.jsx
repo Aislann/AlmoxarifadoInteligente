55ec8504833ee1901a9aec27ca72f108774bfefb
Aislan Oliveira de Carvalho <89170451+Aislann@users.noreply.github.com>
2024-03-10 16:18:44 -0300

    Add files via upload

--- routes.jsx
+++ routes.jsx
@@ -5,6 +5,7 @@ import Requisicao from "./src/components/Requisicao/Requisicao";
 import GestaoProdutos from "./src/components/Produtos/GestaoProdutos";
 import Logs from "./src/components/BenchMarking/Logs";
 import ConfigDados from "./src/components/Configuracoes/ConfigDados";
+import CadastroProdutos from "./src/components/Produtos/CadastroProduto";
 
 export default function routes() {
     return(
@@ -15,6 +16,7 @@ export default function routes() {
                 <Route exect path = '/GestaoProdutos' Component={GestaoProdutos}/>
                 <Route exect path = '/Logs' Component={Logs}/>
                 <Route exect path = '/Configuracoes' Component={ConfigDados}/>
+                <Route exect path = '/CadastroProdutos' Component={CadastroProdutos}/>
             </Routes>
         </BrowserRouter>
     )
