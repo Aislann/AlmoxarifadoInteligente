c07e9fc3eb553aafd188a0ebac19e004557f9a50
Aislan Oliveira de Carvalho <89170451+Aislann@users.noreply.github.com>
2024-03-10 00:07:54 -0300

    Add files via upload

--- src/components/Produtos/GestaoProdutos.jsx
+++ src/components/Produtos/GestaoProdutos.jsx
@@ -1,5 +1,10 @@
 import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
+import { HiOutlineLightBulb } from "react-icons/hi";
+import { FaEdit } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { FiDelete } from "react-icons/fi";
+import { CiPlay1 } from "react-icons/ci";
 
 const GestaoProdutos = () => {
     const [menuAberto, setMenuAberto] = useState(false);
@@ -39,6 +44,72 @@ const GestaoProdutos = () => {
           <span>Gestão de Produtos</span>
         </div>
         <div className='conteudo'>
+        <div className="gestaoProdutos">
+        <button id='BtnInserirItens'>NOVO</button> 
+        <table class="tabelaItens" id="tabelaItens">
+            <tr>
+                <th>Código</th>
+                <th>Produto</th>
+                <th>Preço</th>
+                <th>Estoque Atual</th>
+                <th>Estoque Mínimo</th>
+                <th></th>
+            </tr>
+            <tr>
+        <td>001</td>
+        <td>Produto A</td>
+        <td>R$ 25.00</td>
+        <td>200</td>
+        <td>100</td>
+        <td className='icones'>
+          <HiOutlineLightBulb className='BenchMarking'/>
+          <MdEmail className='enviarEmail'/>
+          <FaEdit className='editar' /> 
+          <FiDelete className='deletar'/>
+          </td>
+    </tr>
+    <tr>
+        <td>002</td>
+        <td>Produto B</td>
+        <td>R$ 15.50</td>
+        <td>200</td>
+        <td>100</td>
+        <td className='icones'>
+          <CiPlay1 className='PlayBench' />
+          <MdEmail className='enviarEmail'/>
+          <FaEdit className='editar'/>
+          <FiDelete className='deletar'/>
+          </td>
+    </tr>
+    <tr>
+        <td>003</td>
+        <td>Produto C</td>
+        <td>R$ 15.50</td>
+        <td>200</td>
+        <td>100</td>
+        <td className='icones'>
+          <HiOutlineLightBulb className='BenchMarking'/>
+          <MdEmail className='enviarEmail'/>
+          <FaEdit className='editar'/>
+          <FiDelete className='deletar'/>
+          </td>
+    </tr>
+    <tr>
+        <td>004</td>
+        <td>Produto Daaaaaaaaaaaaaaa</td>
+        <td>R$ 15.50</td>
+        <td>200</td>
+        <td>100</td>
+        <td className='icones'>
+          <HiOutlineLightBulb className='BenchMarkingFail'/>
+          <MdEmail className='enviarEmail'/>
+          <FaEdit className='editar'/>
+          <FiDelete className='deletar'/>
+          </td>
+    </tr>
+
+          </table>
+          </div>
         </div>
       </div>
     </div>
