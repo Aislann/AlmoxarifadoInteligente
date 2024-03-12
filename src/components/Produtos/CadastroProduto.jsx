c9b6594422f0c6e04f15b5ae6d83d6984d730586
Aislan Oliveira de Carvalho <89170451+Aislann@users.noreply.github.com>
2024-03-12 13:55:22 -0300

    Add files via upload

--- src/components/Produtos/CadastroProduto.jsx
+++ src/components/Produtos/CadastroProduto.jsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import './Gestao.css';
 import NavbarLinks from '../Navbar/NavbarLinks';
+import './CadastroProd.css';
 
 const CadastroProdutos = () => {
     const [menuAberto, setMenuAberto] = useState(false);
@@ -24,61 +25,27 @@ const CadastroProdutos = () => {
             <tr>
                 <th>Código</th>
                 <th>Produto</th>
-                <th>Preço</th>
                 <th>Estoque Atual</th>
                 <th>Estoque Mínimo</th>
             </tr>
-    <tr>
-        <td>002</td>
-        <td>Produto B</td>
-        <td>R$ 15.50</td>
-        <td>200</td>
-        <td>100</td>
-    </tr>
 
-    <tr>
-        <td>004</td>
-        <td>Produto E</td>
-        <td>R$ 50.00</td>
-        <td>200</td>
-        <td>100</td>
-    </tr>
+            <tr>
+                <td>002</td>
+                <td>Produto B</td>
+                <td>200</td>
+                <td>100</td>
+            </tr>
 
-    <tr>
-        <td>005</td>
-        <td>Produto F</td>
-        <td>R$ 19.99</td>
-        <td>200</td>
-        <td>100</td>
-    </tr>
-
-    <tr>
-        <td>006</td>
-        <td>Produto G</td>
-        <td>R$ 9.75</td>
-        <td>200</td>
-        <td>100</td>
-    </tr>
-
-    <tr>
-        <td>007</td>
-        <td>Produto H</td>
-        <td>R$ 12.50</td>
-        <td>200</td>
-        <td>100</td>
-    </tr>
-
-    <tr>
-        <td>008</td>
-        <td>Produto I</td>
-        <td>R$ 35.00</td>
-        <td>200</td>
-        <td>100</td>
-    </tr>
+            <tr>
+            <td>000</td>
+            <td><input type="text" className='addProduto' placeholder='Insira um novo produto...'/></td>
+            <td><input type="number" className='addProduto' placeholder='Insira o estoque atual...'/></td>
+            <td><input type="number" className='addProduto' placeholder='Insira um estoque mínimo...'/></td>
+            </tr>
 
           </table>
-          <Link to={"/GestaoProdutos"} >
-          <button id='BtnInserirItens'>Cadastrar</button>
+          <Link className='cad' to={"/GestaoProdutos"} >
+          <button className='btn-addProd' id='BtnInserirItens'>Cadastrar</button>
           </Link>
           </div>
         </div>
