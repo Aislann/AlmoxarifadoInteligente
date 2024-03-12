74000c63cbc43bebb52b3228f05c395afb17716d
João V. S. Oliveira <145166602+j0t4Vv@users.noreply.github.com>
2024-03-12 16:49:52 -0300

    Add files via upload

--- src/components/Configuracoes/ConfigDados.jsx
+++ src/components/Configuracoes/ConfigDados.jsx
@@ -1,56 +1,56 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './ConfigDadosStyle.css'
+import './ConfigDadosStyle.css';
+import NavbarLinks from '../Navbar/NavbarLinks';
+import { salvarEmail } from './js/email';
+import { salvarWhatsapp } from './js/zap';
 
 const ConfigDados = () => {
-    const [menuAberto, setMenuAberto] = useState(false);
-  
-    const toggleMenu = () => {
-      setMenuAberto(!menuAberto);
+    const [emailVisivel, setEmailVisivel] = useState(true);
+    const [whatsappVisivel, setWhatsappVisivel] = useState(true);
+
+    const TrocarParaInput = (tipo) => {
+        if (tipo === 'email') {
+            setEmailVisivel(false);
+        } else if (tipo === 'whatsapp') {
+            setWhatsappVisivel(false);
+        }
     };
 
-  return (
-    <div>
-    <div className='selection'>
-      <nav className={`navbar ${menuAberto ? 'menu-aberto' : ''}`}>
-        <div className='menu-sanduiche' onClick={toggleMenu}>
-          <div className='barra-menu'></div>
-          <div className='barra-menu'></div>
-          <div className='barra-menu'></div>
+    return (
+        <div>
+            <NavbarLinks/>
+            <div className="main">
+                <div className="header">
+                    <span>Configurações</span>
+                </div>
+                <div className='conteudoConfig'>
+                    <div className='campoEmail'>
+                        {emailVisivel ? (
+                            <button id='trocar' onClick={() => TrocarParaInput('email')}>Adicionar Email</button>
+                        ) : (
+                            <input type="email" placeholder="Digite seu e-mail..." />
+                        )}
+
+                        <button className='add' onClick={salvarEmail} >Adicionar</button>
+
+                    </div>
+                    <div className='campoZap'>
+                        {whatsappVisivel ? (
+                            <button id='trocar' onClick={() => TrocarParaInput('whatsapp')}>Adicionar Whatsapp</button>
+                        ) : (
+                            <input type="tel" placeholder="Digite seu número..." />
+                        )}
+
+                        <button className='add' onClick={salvarWhatsapp} >Adicionar</button>
+                    </div>
+                    <Link to={"/"} className='link-sair'>
+                        <span className='sair'>Sair</span>
+                    </Link>
+                </div>
+            </div>
         </div>
-        <div className='links'>
-          <Link to={"/GestaoProdutos"}>
-            <span>Produtos</span>
-          </Link>
-          <Link to={"/Requisicao"}>
-            <span>Requisição</span>
-          </Link>
-          <Link to={"/Logs"}>
-            <span>BenchMarking Logs</span>
-          </Link>
-        </div>
-      </nav>
-    </div>
-      <div className="main">
-        <div className="header">
-          <span>Configurações</span>
-        </div>
-        <div className='conteudoConfig'>
-          <div className='campoEmail'>
-            <button id='trocar'>Adicionar Email</button>
-            {/* <input type="email" name="AddEmail" id="" placeholder='Adicionar Email'/> */}
-          </div>
-          <div className='campoZap'>
-            <button id='trocar2'>Adicionar Whatsapp</button>
-            {/* <input type="tel" name="AddTel" id="" placeholder='Adicionar Telefone'/> */}
-          </div>
-          <Link to={"/"} className='link-sair'>
-          <span className='sair'>Sair</span>
-          </Link>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default ConfigDados;
