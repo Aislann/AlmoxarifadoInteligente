9a1acfbf5ef30bdeeb148f5bf4de01883d6e9cfb
João V. S. Oliveira <145166602+j0t4Vv@users.noreply.github.com>
2024-03-12 14:30:47 -0300

    Add files via upload

--- src/components/Produtos/CadastroProduto.jsx
+++ src/components/Produtos/CadastroProduto.jsx
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import './Gestao.css';
+import NavbarLinks from '../Navbar/NavbarLinks';
+import './CadastroProd.css';
 
 const CadastroProdutos = () => {
     const [menuAberto, setMenuAberto] = useState(false);
@@ -11,96 +13,24 @@ const CadastroProdutos = () => {
 
   return (
     <div>
-    <div className='selection'>
-      <nav className={`navbar ${menuAberto ? 'menu-aberto' : ''}`}>
-        <div className='menu-sanduiche' onClick={toggleMenu}>
-          <div className='barra-menu'></div>
-          <div className='barra-menu'></div>
-          <div className='barra-menu'></div>
-        </div>
-        <div className='links'>
-          <Link to={"/GestaoProdutos"}>
-            <span>Produtos</span>
-          </Link>
-          <Link to={"/Requisicao"}>
-            <span>Requisição</span>
-          </Link>
-          <Link to={"/Logs"}>
-            <span>BenchMarking Logs</span>
-          </Link>
-          <Link to={"/Configuracoes"}>
-            <span>Configurações</span>
-          </Link>
-          
-        </div>
-      </nav>
-    </div>
+      <NavbarLinks/>
       <div className="main">
         <div className="header">
           <span>Cadastro de Produtos</span>
         </div>
-        <div className='conteudo'>
-        <div className="gestaoProdutos">
-        
-        <table class="tabelaItens" id="tabelaItens">
-            <tr>
-                <th>Código</th>
-                <th>Produto</th>
-                <th>Preço</th>
-                <th>Estoque Atual</th>
-                <th>Estoque Mínimo</th>
-            </tr>
-    <tr>
-        <td>002</td>
-        <td>Produto B</td>
-        <td>R$ 15.50</td>
-        <td>200</td>
-        <td>100</td>
-    </tr>
+        <div className="conteudo">
+        <div className="cadProd">
+          <label htmlFor="name">Nome Produto: </label>
+          <input type="text" className='addProduto' placeholder='Insira um novo produto...'/>
 
-    <tr>
-        <td>004</td>
-        <td>Produto E</td>
-        <td>R$ 50.00</td>
-        <td>200</td>
-        <td>100</td>
-    </tr>
+          <label htmlFor="name">Estoque Atual: </label>
+          <input type="number" className='addProduto' placeholder='Insira a quantidade do estoque atual...'/>
 
-    <tr>
-        <td>005</td>
-        <td>Produto F</td>
-        <td>R$ 19.99</td>
-        <td>200</td>
-        <td>100</td>
-    </tr>
+          <label htmlFor="name">Estoque Mínimo: </label>
+          <input type="number" className='addProduto' placeholder='Insira a quantidade do estoque mínimo...'/>
 
-    <tr>
-        <td>006</td>
-        <td>Produto G</td>
-        <td>R$ 9.75</td>
-        <td>200</td>
-        <td>100</td>
-    </tr>
-
-    <tr>
-        <td>007</td>
-        <td>Produto H</td>
-        <td>R$ 12.50</td>
-        <td>200</td>
-        <td>100</td>
-    </tr>
-
-    <tr>
-        <td>008</td>
-        <td>Produto I</td>
-        <td>R$ 35.00</td>
-        <td>200</td>
-        <td>100</td>
-    </tr>
-
-          </table>
-          <Link to={"/GestaoProdutos"} >
-          <button id='BtnInserirItens'>Cadastrar</button>
+          <Link className='cad' to={"/GestaoProdutos"} >
+            <button className='btn-addProd' id='BtnInserirItens'>Cadastrar</button>
           </Link>
           </div>
         </div>
