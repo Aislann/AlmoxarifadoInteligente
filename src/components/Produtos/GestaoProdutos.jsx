3b5f46f0f76f16742eea8ae3abce112882f1b2fb
Aislan Oliveira de Carvalho <89170451+Aislann@users.noreply.github.com>
2024-03-13 00:53:20 -0300

    Add files via upload

--- src/components/Produtos/GestaoProdutos.jsx
+++ src/components/Produtos/GestaoProdutos.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import './Gestao.css';
 import deletar from '/src/assets/deletar.svg'
@@ -9,9 +9,29 @@ import BenchMarking from '/src/assets/BenchMarking.svg'
 import BenchMarkingFail from '/src/assets/BenchMarkingFail.svg'
 import PlayBench from '/src/assets/PlayBench.svg'
 import NavbarLinks from '../Navbar/NavbarLinks';
+import axios from 'axios';
 
 
 const GestaoProdutos = () => {
+    const [dadosDaApi, setDadosDaApi] = useState([]);
+
+    useEffect(() => {
+      fetch('https://localhost:7226/api/GestaoProdutos')
+        .then(response => response.json())
+        .then(data => setDadosDaApi(data))
+        .catch(error => console.error('Erro ao buscar dados da API:', error));
+    }, []);
+
+    const handleDeletarProduto = async (idProduto) => {
+        try {
+          await axios.delete(`https://localhost:7226/api/GestaoProdutos/${idProduto}`);
+          const novosDados = dadosDaApi.filter(item => item.idProduto !== idProduto);
+          setDadosDaApi(novosDados);
+        } catch (error) {
+          console.error('Erro ao deletar produto:', error);
+        }
+      };
+
   return (
     <div>
     <NavbarLinks/>
@@ -25,66 +45,33 @@ const GestaoProdutos = () => {
         <button id='BtnInserirItens'>NOVO</button> 
         </Link>
         <table class="tabelaItens" id="tabelaItens">
-            <tr>
-                <th>Código</th>
-                <th>Descrição</th>
-                <th>Preço</th>
-                <th>Estoque Atual</th>
-                <th>Estoque Mínimo</th>
-                <th></th>
-            </tr>
-            <tr>
-        <td>001</td>
-        <td>Produto A</td>
-        <td>R$ 00.00</td>
-        <td>200</td>
-        <td>100</td>
-        <td className='icones'>  
-          <img src={PlayBench}/>
-          <img src={enviarEmail}/>
-          <img src={editar}/>
-          <img src={deletar}/>
-          </td>
-    </tr>
-    <tr>
-        <td>002</td>
-        <td>Produto B</td>
-        <td>R$ 00.00</td>
-        <td>200</td>
-        <td>100</td>
-        <td className='icones'>
-          <img src={BenchMarking}/>
-          <img src={emailNone}/>
-          <img src={editar}/>
-          <img src={deletar}/>
-          </td>
-    </tr>
-    <tr>
-        <td>003</td>
-        <td>Produto C</td>
-        <td>R$ 00.00</td>
-        <td>200</td>
-        <td>100</td>
-        <td className='icones'>
-        <img src={BenchMarkingFail}/>
-          <img src={enviarEmail}/>
-          <img src={editar}/>
-          <img src={deletar}/>
-          </td>
-    </tr>
-    <tr>
-        <td>004</td>
-        <td>Produto Teste</td>
-        <td>R$ 00.00</td>
-        <td>200</td>
-        <td>100</td>
-        <td className='icones'>
-        <img src={BenchMarking}/>
-          <img src={emailNone}/>
-          <img src={editar}/>
-          <img src={deletar}/>
-          </td>
-    </tr>
+        <thead>
+        <tr>
+          <th>Código</th>
+          <th>Descrição</th>
+          <th>Preço</th>
+          <th>Estoque Atual</th>
+          <th>Estoque Mínimo</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {dadosDaApi.map(item => (
+          <tr key={item.idProduto}>
+            <td>{item.idProduto}</td>
+            <td>{item.descricao}</td>
+            <td>R$ {item.preco || '00.00'}</td>
+            <td>{item.estoqueAtual}</td>
+            <td>{item.estoqueMinimo}</td>
+            <td className='icones'>  
+              <img src={PlayBench} alt="PlayBench"/>
+              <img src={enviarEmail} alt="Enviar Email"/>
+              <img src={editar} alt="Editar"/>
+              <img src={deletar} alt="Deletar" onClick={() => handleDeletarProduto(item.idProduto)}/>
+            </td>
+          </tr>
+        ))}
+      </tbody>
 
           </table>
           </div>
